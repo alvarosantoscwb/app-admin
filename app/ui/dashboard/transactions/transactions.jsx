130b2b5b34e268f6fef755c56408bcbb12074dda
sleeptd2 <alvarojuniorctb@hotmail.com>
2023-12-21 16:51:18 -0300

    feat: adicionado novos componentes

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,19 +1,103 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./transactions.module.css";
 
 const Transactions = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Ultimas transações</h2>
+      <h2 className={styles.title}>Latest Transactions</h2>
       <table className={styles.table}>
         <thead>
           <tr>
-            <td>Nome</td>
+            <td>Name</td>
             <td>Status</td>
-            <td>Data</td>
-            <td>Quantidade</td>
+            <td>Date</td>
+            <td>Amount</td>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
